6880988bfaa03c8e2fda1b34d50e4015bbbff40f
Oz <osvaldohg@gmail.com>
2017-06-19 17:48:41 -0500

    added new view to the page for books

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -6,5 +6,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
+def index (request):
     return HttpResponse("<h>this is the books homepage22</h>")
+
+
+def detail (request,book_id):
+    return HttpResponse("<h2>details for book id:" + str(book_id)+"</h2>")
+
+

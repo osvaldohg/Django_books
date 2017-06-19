71d1aca10b45a15a653771292e98783ea4cdb593
Oz <osvaldohg@gmail.com>
2017-06-19 15:59:44 -0500

    initial commit of books project

---
+++ mysite/books/models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h>this is the books homepage</h>")
+
+
+class Book (models.Model):
+    name= models.CharField(max_length=100)
+    author= models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+
+
+
+
+

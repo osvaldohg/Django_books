fd4210820bc442da6e2fddfc4adbe10ff6180be7
Oz <osvaldohg@gmail.com>
2017-06-20 22:48:10 -0500

    added namespaces for app books

--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from . import views
 
+app_name='books'
 urlpatterns = [
     url(r'^$',views.index, name='index'),
     url(r'^(?P<book_id>[0-9]+)/$',views.detail, name='detail'),

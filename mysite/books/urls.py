0b187cb43e3615dfe6d7f25efcc867734ef5801e
Oz <osvaldohg@gmail.com>
2017-06-22 11:16:10 -0500

    changing model to use generic views

--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 app_name='books'
 urlpatterns = [
-    url(r'^$',views.index, name='index'),
-    url(r'^(?P<book_id>[0-9]+)/$',views.detail, name='detail'),
+    url(r'^$',views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(), name='detail'),
 ]
--- mysite/books/views.py
+++ mysite/books/views.py
@@ -5,27 +5,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views import generic
 from .models import Book
-from django.shortcuts import render
-from django.http import Http404
+
+class IndexView(generic.ListView):
+    template_name='books/index.html'
+
+    def get_queryset(self):
+        return Book.objects.all()
 
 
-def index (request):
-    all_books=Book.objects.all()
 
-    context={
-            'all_books':all_books
-    }
-
-    return render(request,'books/index.html',context)
-
-
-def detail (request,book_id):
-    try :
-        book=Book.objects.get(id=book_id)
-    except Book.DoesNotExist:
-        raise Http404('This book does not exist')
-    return render(request,'books/detail.html',{'book':book})
+class DetailView(generic.DetailView):
+    model = Book
+    template_name='books/detail.html'
 
 
 
